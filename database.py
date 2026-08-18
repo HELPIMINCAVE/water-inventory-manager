8275ce033a57b731e17a0e316586692d6b52a89f
helpimincave <joseguiang484@gmail.com>
2026-08-18 20:13:31 +0800

    Fixed another bug on SQL

--- database.py
+++ database.py
@@ -31,6 +31,19 @@ class Database:
             """
             )
             
+            cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS products (
+                                product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                user_id INTEGER NOT NULL DEFAULT 1,
+                                name TEXT NOT NULL,
+                                quantity INTEGER DEFAULT 0,
+                                empty_quantity INTEGER DEFAULT 0,
+                                unit_price REAL DEFAULT 0.0,
+                                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                FOREIGN KEY (user_id) REFERENCES users(user_id)
+                            )
+                        """)
+            
             cursor.execute(
                 """
                 CREATE TABLE IF NOT EXISTS products (
